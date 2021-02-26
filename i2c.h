c22cf7c44a2babc39560182c8ea1a7f18cb8aed8
amartinezacosta <amartinezacosta@miners.utep.edu>
2021-02-26 10:35:33 -0700

    Licensing info added

--- i2c.h
+++ i2c.h
@@ -1,3 +1,7 @@
+/*
+Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
+*/
+
 #ifndef I2C_H_
 #define I2C_H_
 
--- server.h
+++ server.h
@@ -1,9 +1,6 @@
 /*
- * server.h
- *
- *  Created on: Aug 26, 2019
- *      Author: AMLUser
- */
+Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
+*/
 
 #ifndef SERVER_H_
 #define SERVER_H_
